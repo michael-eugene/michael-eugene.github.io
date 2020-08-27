808940f7f5cadef4ab6a27bef0d08e762ce1c3db
Michael <eugenemichael1999@gmail.com>
2020-08-27 19:11:53 +0100

    squashed bugs caused by GSAP

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ feather.replace()
 gsap.from('#title', { delay: 1.5, y: -50, opacity: 0, duration: 1 }) // Title
 gsap.from('#about', { delay: 2.2, y: 50, opacity: 0, duration: 1.2})
 gsap.from('#socials', { delay: 2.2, y: 30, opacity: 0, duration: 1.3})
-gsap.from('#section', { delay: 4, x: -50, opacity: 0, duration: 1.5})
+gsap.from('#section', { delay: 4, opacity: 0, duration: 1.5})
 
 // VUE INSTANCE
 new Vue({
