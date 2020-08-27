f8fd960c696a8b1eb324f58cda23aa03bfe9db06
Michael <eugenemichael1999@gmail.com>
2020-08-27 18:59:44 +0100

    added animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,15 @@ gsap.from('#title', { delay: 1.5, y: -50, opacity: 0, duration: 1 }) // Title
 gsap.from('#about', { delay: 2.2, y: 50, opacity: 0, duration: 1.2})
 gsap.from('#socials', { delay: 2.2, y: 30, opacity: 0, duration: 1.3})
 gsap.from('#section', { delay: 4, x: -50, opacity: 0, duration: 1.5})
+
+// VUE INSTANCE
+new Vue({
+  el: '#projects',
+  data: {},
+  methods: {
+    openModal: function (e) {
+      e.currentTarget.classList.toggle('modal')
+      e.currentTarget.classList.toggle('rectangle')
+    }
+  }
+})
